5d5fc984c4ba4c132bc2e6327d2bc2d7df904598
Ruan Ferreira <8647151+ruan4f@users.noreply.github.com>
2022-01-26 00:54:48 -0300

    Change test

--- iniciando-react/src/App.tsx
+++ iniciando-react/src/App.tsx
@@ -1,5 +1,28 @@
+import { useState } from "react";
+import { Tweet } from "./components/Tweet";
+
 function App() {
-  return <h1>Hello World</h1>
+  const [tweets, setTweets] = useState<string[]>([])
+
+  const [countTweets, setCountTweets] = useState<number>(1)
+
+  function createTweet() {
+    setCountTweets(countTweets+1);
+    const tweet = `Tweet ${countTweets}`;
+    setTweets([...tweets, tweet]);
+  }
+
+  return (
+    <div>
+      {
+        tweets.map(tweet => {
+          return <Tweet text={tweet} />
+        })
+      }
+
+      <button onClick={createTweet}>Adicionar Tweet</button>
+    </div>
+  );
 }
 
 export default App
